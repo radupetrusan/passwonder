bf7429755b5a76c6b081e99e191b61374ca8eb3b
radu.petrusan@yahoo.com <radu.petrusan@yahoo.com>
2019-06-11 22:00:52 +0300

    Correct prod environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyDSTLjoRcd3riXPiCl8Wm6Yo85ZrBh9brc',
+    authDomain: 'passwonder-app.firebaseapp.com',
+    databaseURL: 'https://passwonder-app.firebaseio.com',
+    projectId: 'passwonder-app',
+    storageBucket: 'passwonder-app.appspot.com',
+    messagingSenderId: '993595874456',
+    appId: '1:993595874456:web:facc6fcabbab9244'
+  }
 };
